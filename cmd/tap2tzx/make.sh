e4a9fbc6a2114ccd92b3fbb1d0ed4e1bb2d27a20
ha1tch <bitdrome@gmail.com>
2025-01-18 17:37:07 -0300

    More directory reorg, makescript update

--- cmd/tap2tzx/make.sh
+++ cmd/tap2tzx/make.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Ensure we're in the tap2tzx directory
+cd "$(dirname "$0")"
+
 # Initialize module if go.mod doesn't exist
 if [ ! -f go.mod ]; then
     go mod init tap2tzx
@@ -8,10 +11,12 @@ fi
 # Get dependencies
 go get gopkg.in/yaml.v3
 
-# Build
-GOOS=windows GOARCH=amd64 go build -o ../bin/tap2tzx.exe
-GOOS=darwin  GOARCH=arm64 go build -o ../bin/tap2tzx.mac
-GOOS=linux   GOARCH=amd64 go build -o ../bin/tap2tzx.linux
+# Build into the main bin directory (two levels up)
+echo "Building tap2tzx..."
+GOOS=windows GOARCH=amd64 go build -o ../../bin/tap2tzx.exe
+GOOS=windows GOARCH=386   go build -o ../../bin/tap2tzx.win32.exe
+GOOS=linux   GOARCH=amd64 go build -o ../../bin/tap2tzx.linux
+GOOS=darwin  GOARCH=arm64 go build -o ../../bin/tap2tzx.mac
 
 # Make executable
-chmod +x ../bin/tap2tzx.*
+chmod +x ../../bin/tap2tzx.*
--- mk.sh
+++ mk.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 mkdir -p bin
 
-GOOS=windows GOARCH=amd64 go build -o bin/maketap.exe   maketap.go
-GOOS=windows OOARCH=i386  go build -o bin/maketap.win32.exe maketap.go
-GOOS=linux   GOARCH=amd64 go build -o bin/maketap.linux maketap.go
-GOOS=darwin  GOARCH=arm64 go build -o bin/maketap.mac   maketap.go
+# Build maketap for different platforms
+echo "Building maketap..."
+GOOS=windows GOARCH=amd64 go build -o bin/maketap.exe   ./cmd/maketap
+GOOS=windows GOARCH=386   go build -o bin/maketap.win32.exe ./cmd/maketap
+GOOS=linux   GOARCH=amd64 go build -o bin/maketap.linux ./cmd/maketap
+GOOS=linux   GOARCH=386   go build -o bin/maketap.linux32 ./cmd/maketap
+GOOS=darwin  GOARCH=arm64 go build -o bin/maketap.mac   ./cmd/maketap
 
-GOOS=windows GOARCH=amd64 go build -o bin/loadtap.exe   loadtap.go
-GOOS=windows GOARCH=i386  go build -o bin/loadtap.win32.exe loadtap.go
-GOOS=linux   GOARCH=amd64 go build -o bin/loadtap.linux loadtap.go
-GOOS=darwin  GOARCH=arm64 go build -o bin/loadtap.mac   loadtap.go
+# Build loadtap for different platforms
+echo "Building loadtap..."
+GOOS=windows GOARCH=amd64 go build -o bin/loadtap.exe   ./cmd/loadtap
+GOOS=windows GOARCH=386   go build -o bin/loadtap.win32.exe ./cmd/loadtap
+GOOS=linux   GOARCH=amd64 go build -o bin/loadtap.linux ./cmd/loadtap
+GOOS=linux   GOARCH=386   go build -o bin/loadtap.linux32 ./cmd/loadtap
+GOOS=darwin  GOARCH=arm64 go build -o bin/loadtap.mac   ./cmd/loadtap
+
+# Build tap2tzx (from its own directory)
+echo "Building tap2tzx..."
+(cd cmd/tap2tzx && ./mk.sh)
