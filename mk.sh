ff34896a0f5cc068be5957e701a160ff7dc72754
ha1tch <bitdrome@gmail.com>
2025-01-18 18:17:59 -0300

    Binary releases fix

--- mk.sh
+++ mk.sh
@@ -11,11 +11,11 @@ GOOS=darwin  GOARCH=arm64 go build -x -o ../bin/maketap.mac       maketap.go
 popd
 
 pushd cmd/loadtap
-GOOS=windows GOARCH=amd64 go build -x -o bin/loadtap.exe          loadtap.go
-GOOS=windows GOARCH=386   go build -x -o bin/loadtap.win32.exe    loadtap.go
-GOOS=linux   GOARCH=amd64 go build -x -o bin/loadtap.linux        loadtap.go
-GOOS=linux   GOARCH=386   go build -x -o bin/loadtap.linux32      loadtap.go
-GOOS=darwin  GOARCH=arm64 go build -x -o bin/loadtap.mac          loadtap.go
+GOOS=windows GOARCH=amd64 go build -x -o ../bin/loadtap.exe          loadtap.go
+GOOS=windows GOARCH=386   go build -x -o ../bin/loadtap.win32.exe    loadtap.go
+GOOS=linux   GOARCH=amd64 go build -x -o ../bin/loadtap.linux        loadtap.go
+GOOS=linux   GOARCH=386   go build -x -o ../bin/loadtap.linux32      loadtap.go
+GOOS=darwin  GOARCH=arm64 go build -x -o ../bin/loadtap.mac          loadtap.go
 popd
 
 (pushd cmd/tap2tzx && ./mk.sh)
